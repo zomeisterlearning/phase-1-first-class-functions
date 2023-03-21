adc643064120365afd5b7ea1bf4c916a734bd4cf
Zoe Guidry <oyezoey@gmail.com>
2023-03-21 14:01:56 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback){
+  callback();
+}
+
+function returnsANamedFunction() {
+  return receivesAFunction;
+}
+function returnsAnAnonymousFunction() {
+  return ()=>{};
+}
+
